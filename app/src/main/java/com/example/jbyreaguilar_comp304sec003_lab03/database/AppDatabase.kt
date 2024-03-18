90eb4c9ff3370c3178d2379713bbad6dd0e035fe
DM Brar <dmvbrar@gmail.com>
2024-03-17 20:56:00 -0400

    added appdatabase file

---
+++ app/src/main/java/com/example/jbyreaguilar_comp304sec003_lab03/database/AppDatabase.kt
@@ -0,0 +1,33 @@
+package com.example.jbyreaguilar_comp304sec003_lab03.database
+
+import Schedule
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.jbyreaguilar_comp304sec003_lab03.database.schedule.ScheduleDao
+
+@Database(entities = [Schedule::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun scheduleDao(): ScheduleDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "airline_schedule.db")
+                    // .createFromAsset("database/your_prepopulated_database.db") // Uncomment if you have a prepopulated database
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/jbyreaguilar_comp304sec003_lab03/database/schedule/ScheduleDao.kt
+++ app/src/main/java/com/example/jbyreaguilar_comp304sec003_lab03/database/schedule/ScheduleDao.kt
@@ -8,9 +8,9 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface ScheduleDao {
 
-    @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
+    @Query("SELECT * FROM schedule ORDER BY arrivalTime ASC")
     fun getAllSchedules(): Flow<List<Schedule>>
 
-    @Query("SELECT * FROM schedule WHERE airline_name = :airlineName ORDER BY arrival_time ASC")
+    @Query("SELECT * FROM schedule WHERE airlineName = :airlineName ORDER BY arrivalTime ASC")
     fun getSchedulesByAirlineName(airlineName: String): Flow<List<Schedule>>
 }
